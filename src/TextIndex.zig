1c13400112c48368b54e2c09167e0516cda9e0ed
Markus Korn <markus.korn@gmail.com>
2025-01-05 19:53:56 +0100

    Add advanced query support with customization options
    - Introduce query capabilities with customizable options using `QueryType` enum and `QueryOptions` struct.
    - Implement basic search functionality with logical AND/OR operations.
    - Add tests to validate OR and AND query operations.

--- src/TextIndex.zig
+++ src/TextIndex.zig
@@ -8,6 +8,12 @@ const TokenIter = @import("TokenIter.zig");
 const TextIterator = SliceIterator(u64);
 const TextFilter = Filter(.{}, []const u8, *TextIterator);
 
+const QueryType = enum { All, Any };
+
+const QueryOptions = struct {
+    query_type: QueryType = .Any,
+};
+
 const TextOptions = struct {
     estimated_keys: usize = 100,
     language: []const u8 = "german",
@@ -78,6 +84,42 @@ pub fn contains(self: *Self, word: []const u8) !bool {
     return self.filter.contains(key);
 }
 
+pub fn query(self: *Self, term: []const u8, results: *std.ArrayListUnmanaged([]const u8), options: QueryOptions) !usize {
+    var tokens = try TokenIter.init(self.alloc, term);
+    defer tokens.deinit();
+    var keys = std.ArrayListUnmanaged(u64){};
+
+    // TODO: use hashmap ??
+    while (tokens.next()) |token| {
+        const s = self.stemmer.stem(token);
+        try keys.append(self.alloc, std.hash_map.hashString(s));
+    }
+
+    const ts = try keys.toOwnedSlice(self.alloc);
+    errdefer self.alloc.free(ts);
+
+    try self.cache.append(self.alloc, ts);
+
+    switch (options.query_type) {
+        .Any => {
+            return try self.filter.queryLogicalOr(
+                self.alloc,
+                ts,
+                *std.ArrayListUnmanaged([]const u8),
+                results,
+            );
+        },
+        .All => {
+            return try self.filter.queryLogicalAnd(
+                self.alloc,
+                ts,
+                *std.ArrayListUnmanaged([]const u8),
+                results,
+            );
+        },
+    }
+}
+
 test "index contains words" {
     var ti = try Self.init(std.testing.allocator, .{});
     defer ti.deinit();
@@ -110,4 +152,49 @@ test "invalid contains words" {
 
     try std.testing.expectError(error.MoreThanOneWord, ti.contains("Hallo googog"));
     try std.testing.expectError(error.EmptySearchWord, ti.contains("        "));
+}
+
+test "simple OR query" {
+    var ti = try Self.init(std.testing.allocator, .{});
+    defer ti.deinit();
+
+    const p1 = try ti.insert("Hallo welt");
+    defer std.testing.allocator.destroy(p1);
+
+    const p2 = try ti.insert("dies ist ein test");
+    defer std.testing.allocator.destroy(p2);
+
+    try ti.index();
+
+    var results = std.ArrayListUnmanaged([]const u8){};
+    defer results.deinit(std.testing.allocator);
+
+    const num = try ti.query("Hallo test", &results, .{});
+
+    try std.testing.expectEqual(2, num);
+
+    results.clearRetainingCapacity();
+
+    const num2 = try ti.query("boo", &results, .{});
+    try std.testing.expectEqual(0, num2);
+}
+
+test "simple AND query" {
+    var ti = try Self.init(std.testing.allocator, .{});
+    defer ti.deinit();
+
+    const p1 = try ti.insert("Hallo welt test");
+    defer std.testing.allocator.destroy(p1);
+
+    const p2 = try ti.insert("dies ist ein test");
+    defer std.testing.allocator.destroy(p2);
+
+    try ti.index();
+
+    var results = std.ArrayListUnmanaged([]const u8){};
+    defer results.deinit(std.testing.allocator);
+
+    const num = try ti.query("Hallo test", &results, .{ .query_type = .All });
+
+    try std.testing.expectEqual(1, num);
 }
